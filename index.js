9b1312396d18b9a66d16526e717206eb11529e88
Blake Sticland <blakelists@yahoo.com.au>
2021-02-02 15:01:59 +1100

    recursive loop for questions is working

--- index.js
+++ index.js
@@ -10,221 +10,195 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const { number } = require('yargs');
+const { run } = require('jest');
+
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// Define new instance of each Class
-const employee = new Employee();
-const manager = new Manager();
-const engineer = new Engineer();
-const intern = new Intern();
 
 // TODO: Create an an array of questions for user input
 // TODO: Try a recursive prompt to allow user to choose when to exit prompt.
 // Inquirer.js/packages/inquirer/examples/recursive.js
 
-// Make variables needed to store answers.
-const managerOutput = [];
-const engineerOutput = [];
-const internOutput = [];
-
-// Create inital questions to ask the Manager.
-const initialQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is your team manager name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your employee id?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email address?"
-    },
-    {
-        type: "input",
-        name: "office_number",
-        message: "What is your office number?"
-    }, 
-];
-
-const engineerQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the Engineer's name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is their employee id?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is their email address?"
-    },
-    {
-        type: "input",
-        name: "githubUsername",
-        message: "What is their Github Username?"
-    }, 
-];
-const internQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the Intern's name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is their employee id?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is their email address?"
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "What is the name of their school?"
-    }, 
-];
-const finishQuestion = [
-    {
-        type: "list",
-        name: "team-member-menu",
-        message: "Would you lke to add a team member or finish building your team?",
-        choices: ["Engineer",
-            "Intern",
-            "Finish building team"]
-    },    
-
-]
-
 // Use Inquirer to ask questions. 
 // TODO: Create a recursive loop based on the user choice from finishQuestion list
 
-function askManager() {
-    inquirer.prompt(initialQuestions).then((managerAnswers) => {
-        managerOutput.push(managerAnswers.name);
-    });
-};
+// TODO -------------- Chris's code start
 
-function ask() {
-    inquirer.prompt(finishQuestion);
-    if (answers.team-member-menu === "Engineer") {
-        inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
-            engineerOutput.push(engineerAnswers.name);
-            ask();
+class Questions {
+    constructor() {
+        this.team = [];
+    }
+    start() {
+        // initialising
+        this.anotherTeamMember();
+    }
+    
+    anotherTeamMember() {
+        inquirer.prompt([
+            // Add a team member?
+            {
+                type: "confirm",
+                name: "addTeamMember",
+                message: "Would you like to add a team member?"
+            }
+        ]).then((response) => {
+            if (response.addTeamMember) {
+                this.whatTypeOfEmployee();
+            } else {
+                this.generateHtml();
+            }
         });
     }
-    else if (answers.team-member-menu === "Intern"){
-        inquirer.prompt(internQuestions).then((internAnswers) => {
-            internOutput.push(internAnswers.name);
-            ask();
+    whatTypeOfEmployee() {
+        inquirer.prompt([
+            // What type of employee? 
+            {
+                type: "list",
+                name: "type",
+                message: "Would you lke to add a team member or finish building your team?",
+                choices: [
+                    "Manager",
+                    "Engineer",
+                    "Intern",
+                ]
+            } 
+        ]).then((response) => {
+            if (response.type === "Manager") {
+                this.askManagerQuestions();
+            } else if (response.type === "Engineer") {
+                this.askEngineerQuestions();
+            } else if (response.type === "Intern") {
+                this.askInternQuestions();
+            } 
+            //etc
         });
     }
-    else if (answers.team-member-menu === "Finish building team"){
-        return;
+    
+    askManagerQuestions() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is your team manager name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your employee id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your email address?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is your office number?"
+            }       
+        ]).then((response) => {
+            let manager = new Manager(response.name, response.id, response.email, response.officeNumber); // { name: "", type: "" }
+            this.team.push(manager);
+            this.anotherTeamMember()
+        })
     }
-};
-function askQuestions() {
-    askManager();
-    ask();
-}; // end of ask() Function
+    
+    askEngineerQuestions() {
+        inquirer.prompt([
+            // engineer questions
+            {
+                type: "input",
+                name: "name",
+                message: "What is the Engineer's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is their employee id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is their email address?"
+            },
+            {
+                type: "input",
+                name: "githubUsername",
+                message: "What is their Github Username?"
+            }, 
+        ]).then((response) => {
+            let engineer = new Engineer(response.name, response.id, response.email, response.githubUsername); // { name: "", type: "" }
+            this.team.push(engineer);
+            this.anotherTeamMember()
+        })
+    }
+    
+    askInternQuestions() {
+        inquirer.prompt([
+            // intern questions
+            {
+                type: "input",
+                name: "name",
+                message: "What is the Intern's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is their employee id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is their email address?"
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is the name of their school?"
+            }, 
+        ]).then((response) => {
+            let intern = new Intern(response.name, response.id, response.email, response.school); // { name: "", type: "" }
+            this.team.push(intern);
+            this.anotherTeamMember()
+        })
+    }
+    
+    
+    generateHtml() {
+        console.log("generateHtml has been run");
+        // loop through this.team
+        this.team.forEach(element => console.log(element));
+        // create cards for each employee 
+        // add it to overall html temlplate
+        // fs.writeFile("filename.html", generatedHtml, () => {
+        //})
+    }
+}
 
+const questions = new Questions();
 
-// const askQuestions = () => {
-//     return inquirer.prompt([
-        // {
-        //     type: "input",
-        //     name: "name",
-        //     message: "What is your team manager name?"
-        // },
-        // {
-        //     type: "input",
-        //     name: "id",
-        //     message: "What is your employee id?"
-        // },
-        // {
-        //     type: "input",
-        //     name: "email",
-        //     message: "What is your email address?"
-        // },
-        // {
-        //     type: "input",
-        //     name: "office_number",
-        //     message: "What is your office number?"
-        // }, 
-    //     {
-    //         type: "list",
-    //         name: "team-member-menu",
-    //         message: "Would you lke to add a team member or finish building your team?",
-    //         choices: ["Engineer",
-    //             "Intern",
-    //             "Finish building team"]
-    //     }      
-    // ]);
-// };
-// const answers = askQuestions();
+questions.start();
 
-// switch (answers.role !== "none") {
-//         case (answers.role === "Engineer"):
-//         const askEngineerQuestions = () => {
-//             return inquirer.prompt([
-//                 {
-//                     type: "input",
-//                     name: "githubUsername",
-//                     message: "What is your GithHub Username?"
-//                 }
-//             ])
-//         }
-//         const answersEngineer = await askEngineerQuestions();
-//         const githubUsername = answersEngineer.githubUsername;
-//         break;
-        
-//         case (answers.role === "Intern"):
-//         const askEngineerQuestions = () => {
-//             return inquirer.prompt([
-//                 {
-//                     type: "input",
-//                     name: "githubUsername",
-//                     message: "What is your GithHub Username?"
-//                 }
-//             ])
-//         }
-//         const answersIntern = await askInternQuestions();
-//         const school = answersIntern.school;
-//         break;
-
-//         default:
-//             return
-// }
+// TODO Chris's code finish ----------------------------
 
 // TODO: Create a function to initialize app
-const init = async () => {
-    try {
+// const init = async () => {
+//     try {
   
-      const answers = await askQuestions();
+//       const answers = await askQuestions();
   
-      const generateTemplate = generateTemplate(answers);
+//       const generateTemplate = generateTemplate(answers);
   
-      await writeFileAsync(`./dist/${answers.name.split(' ').join('_')}_README.md`, generateTemplate.trim());
+//       await writeFileAsync(`./dist/${answers.name.split(' ').join('_')}_README.md`, generateTemplate.trim());
   
-      console.log('successfully wrote file')
+//       console.log('successfully wrote file')
   
-    } catch (err) {
-      console.log(err);
-    }
-  };
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
   
-  // Function call to initialize app
-  init();
+//   // Function call to initialize app
+//   init();
   
